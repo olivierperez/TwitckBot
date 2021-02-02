cef239939136b5a44c2b6ab30c92ba61b4850f4e
Olivier PEREZ <olivier@olivierperez.fr>
2021-02-02 16:56:56 +0100

    Create Gradle tasks to package TwitckBot

---
+++ sample/TwitckBot.bat
@@ -0,0 +1,3 @@
+@echo off
+java -version
+java -Dfile.encoding=UTF-8 -jar TwitckBot.jar --oauth-token <OAUTH_TOKEN> --host-name <HOST_NAME>
---
+++ sample/TwitckBot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+java -version
+java -Dfile.encoding=UTF-8 -jar TwitckBot.jar --oauth-token <OAUTH_TOKEN> --host-name <HOST_NAME>
