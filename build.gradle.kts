1df78be3f50bf26705e3798aa3a31abd411877f8
Olivier PEREZ <olivier@olivierperez.fr>
2021-02-09 21:17:09 +0100

    chore(build): bump to Kotlin 1.4.30

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.21"
-    kotlin("kapt") version "1.4.21"
+    kotlin("jvm") version "1.4.30"
+    kotlin("kapt") version "1.4.30"
 }
 
 repositories {
